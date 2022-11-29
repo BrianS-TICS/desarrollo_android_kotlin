9a81b0ad740041400e8e55161451c115cd9df3b6
Brian Sanchez Aguilar <briansanchezaguilar@gmail.com>
2022-11-28 23:47:50 -0700

    Se agrega peticion

--- ListaPersonalizada/app/src/main/java/com/example/listapersonalizada/MainActivity.kt
+++ ListaPersonalizada/app/src/main/java/com/example/listapersonalizada/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.listapersonalizada
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val url = "https://randomuser.me/api/?results=25"
+
+        val listaProductos : MutableList<Producto> = mutableListOf()
+        val cola = Volley.newRequestQueue(this)
+        val solicitud  = StringRequest(Request.Method.GET, url,{ response ->
+
+        },{})
+        /*
         val producto1 = Producto("Cámara", 100.23, "Cámara diseñada para tomar fotos regulares", R.drawable.camara)
         val producto2 = Producto("Cámara compleja", 200.23, "Cámara diseñada para tomar fotos profecionales" ,R.drawable.camara)
 
@@ -17,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         val adapter = ProductosAdapter(this, listaProductos)
 
         lista.adapter = adapter
+        */
         // Cuando damos click a un elemento de la lista]
         lista.setOnItemClickListener { parent, view, position, id ->
             val intent = Intent(this, ProductoActivity::class.java)
--- ListaPersonalizada/app/src/main/java/com/example/listapersonalizada/Producto.kt
+++ ListaPersonalizada/app/src/main/java/com/example/listapersonalizada/Producto.kt
@@ -1,5 +1,5 @@
 package com.example.listapersonalizada
 
-class Producto(val nombre: String, val precio : Double, val descripcion : String, val imagen : Int):java.io.Serializable{
+class Producto(val nombre: String, val precio : Double, val descripcion : String, val imagen : Int, val foto : String):java.io.Serializable{
 
 }
